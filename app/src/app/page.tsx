542f76aa3efb755128a5e1f1e82a87f78d0e35da
ZooSOS <wbin90620@gmail.com>
2024-10-07 15:05:01 -0700

    fixed queryClientProvider

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import React from 'react'
-import {  createConfig } from '@wagmi/core'
+import { createConfig } from '@wagmi/core'
 import { WagmiProvider, http } from 'wagmi'
 import { luxMainnet, luxTestnet, luxDevnet, luxChaosnet } from './luxChains'
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Home from './home'
 
 const config = createConfig({
@@ -15,11 +16,17 @@ const config = createConfig({
     [luxChaosnet.id]: http()
   }
 })
+
+const queryClient = new QueryClient()
 const Page = () => {
   return (
-    <WagmiProvider config={config}>
-        <Home />
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={config}>
+        <ConnectKitProvider>
+          <Home />
+        </ConnectKitProvider>
+      </WagmiProvider>
+    </QueryClientProvider>
   )
 }
 
